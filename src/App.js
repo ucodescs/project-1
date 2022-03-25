c209980d9741a5bd1361d8336440b52c9c7078e9
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-03-25 17:22:12 +0000

    Lesson 13

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import './App.css';
 import {Component} from 'react'
 
 class App extends Component{
+  state ={
+      name: 'Renato Ribeiro',
+      counter: 0
+    };
+  
+
+  handlePClick = (event) =>{
+    this.setState({name: 'Renato Miguel da Cruz Ribeiro'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1 });
+  }
+
   render(){
+    const {name, counter} =this.state;
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+
+          <p onClick={this.handlePClick}>
+            {name} {counter}
           </p>
+          
           <a
+          onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Este é o link {counter}
           </a>
         </header>
       </div>
