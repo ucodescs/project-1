22e1248adfefc6c1aef33ff5cde77ac690099785
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-03-26 15:39:56 +0000

    Lesson 15

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react'
 
 class App extends Component{
   state ={
-      name: 'Renato Ribeiro',
-      counter: 0
+      posts: [
+        {
+          id: 1,
+          title: 'O titulo 1',
+          body: 'o corpo 1'
+        },
+        {
+          id: 2,
+          title: 'O titulo 2',
+          body: 'o corpo 2'
+        },
+        {
+          id: 3,
+          title: 'O titulo 3',
+          body: 'o corpo 3'
+        },
+      ]
     };
   
 
-  handlePClick = (event) =>{
-    this.setState({name: 'Renato Miguel da Cruz Ribeiro'});
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter + 1 });
-  }
-
   render(){
-    const {name, counter} =this.state;
+    const {posts} =this.state;
     
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          
-          <a
-          onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link {counter}
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1 >{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
