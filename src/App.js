49e658c32ab5569921144a7a5d9f0c8615c0f73a
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-03-26 16:07:41 +0000

    Lesson 16

--- src/App.js
+++ src/App.js
@@ -2,32 +2,55 @@ import './App.css';
 import {Component} from 'react'
 
 class App extends Component{
-  state ={
-      posts: [
-        {
-          id: 1,
-          title: 'O titulo 1',
-          body: 'o corpo 1'
-        },
-        {
-          id: 2,
-          title: 'O titulo 2',
-          body: 'o corpo 2'
-        },
-        {
-          id: 3,
-          title: 'O titulo 3',
-          body: 'o corpo 3'
-        },
-      ]
-    };
+  state = {
+      counter: 0,
+      posts: [{
+        id: 1,
+        title: 'O titulo 1',
+        body: 'o corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'o corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'o corpo 3'
+      },]
+  };
   
+  timeOutUpdate = null;
 
+  componentDidMount(){
+    this.handleTimeOut()  
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut()
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+
+  handleTimeOut = () =>{
+    const {posts, counter} = this.state;
+    posts[0].title = 'Novo titulo';
+    
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({posts, counter: counter+1});
+    }, 1000);
+  }
+  
   render(){
-    const {posts} =this.state;
+    const {posts, counter} =this.state;
     
     return (
       <div className="App">
+        <p>{counter}</p>
         {posts.map(post => (
           <div key={post.id}>
             <h1 >{post.title}</h1>
